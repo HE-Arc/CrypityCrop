78c9705490ce8625a404c824e50dfd40881acc67
Luca Campana <luca.campana@he-arc.ch>
2021-12-02 15:41:28 +0100

    Created Controllers + models & configurated the models

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function vaults()
+    public function vault()
     {
         return $this->belongsToMany(Vault::class);
     }
