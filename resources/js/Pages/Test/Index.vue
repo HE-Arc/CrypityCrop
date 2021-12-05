bfe48381b944207bbda9b3f5e96c46b758bf8b2a
Thierry Flück <thierry.fluck@he-arc.ch>
2021-12-05 15:55:46 +0100

    First controller and view test with static data

---
+++ resources/js/Pages/Test/Index.vue
@@ -0,0 +1,43 @@
+<template>
+    <Head title="Liste des livres" />
+
+    <breeze-authenticated-layout>
+        <h2 class="h4 font-weight-bold">
+            Liste des livres
+            {{display()}}
+            <CredentialsTable :folder="folder"></CredentialsTable>
+        </h2>
+
+    </breeze-authenticated-layout>
+</template>
+
+<script>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
+import CredentialsTable from '@/Components/Table/CredentialsTable.vue'
+import { Head, Link } from '@inertiajs/inertia-vue3'
+import { Inertia } from '@inertiajs/inertia'
+
+export default {
+    components: {
+        BreezeAuthenticatedLayout,
+        Head,
+        CredentialsTable,
+    },
+    props: {
+        'folder': {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        display() {
+            console.log("TESTs : ")
+            //console.log(folder)
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
